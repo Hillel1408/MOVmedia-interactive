2f09ffac6fc96a4bdebe57f632658d68876f0032
Hillel1408 <etemax@bk.ru>
2026-05-16 13:41:58 +0300

    +

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,3 +1,9 @@
 export default function Intro() {
-  return <div>Intro</div>;
+  return (
+    <div className="h-screen bg-[url('assets/images/map.png')] bg-cover bg-center pt-22.5">
+      <div className="max-w-277.5">
+        <h1>Город содружества</h1>
+      </div>
+    </div>
+  );
 }
